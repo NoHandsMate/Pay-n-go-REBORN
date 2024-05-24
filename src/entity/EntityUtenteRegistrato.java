c66d002e3630e9e083fd38306cd7b2ae0e27b5b3
Matteo Arnese <68701124+xMattMaster@users.noreply.github.com>
2024-05-24 23:38:18 +0200

    Popolamento EntityUtenteRegistrato Buonanotte.

--- src/entity/EntityUtenteRegistrato.java
+++ src/entity/EntityUtenteRegistrato.java
@@ -1,12 +1,20 @@
 package entity;
 
+import database.ClienteRegistratoDAO;
+
+import java.util.List;
+
 public class EntityUtenteRegistrato {
-    private final String nome;
-    private final String cognome;
-    private final String contattoTelefonico;
-    private final String email;
-    private final String automobile;
-    private final int numPostiDisponibili;
+    private String nome;
+    private String cognome;
+    private String contattoTelefonico;
+    private String email;
+    private String automobile;
+    private int numPostiDisponibili;
+    private List<EntityViaggio> viaggiCondivisi;
+    private List<EntityPrenotazione> prenotazioni;
+    private List<EntityValutazione> valutazioni;
+
 
     public EntityUtenteRegistrato() {
         this.nome = "";
@@ -24,5 +32,81 @@ public class EntityUtenteRegistrato {
         this.email = email;
         this.automobile = automobile;
         this.numPostiDisponibili = numPostiDisponibili;
+    }
+
+    public EntityUtenteRegistrato(ClienteRegistratoDAO clienteRegistratoDAO) {
+        /* TODO */
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getContattoTelefonico() {
+        return contattoTelefonico;
+    }
+
+    public void setContattoTelefonico(String contattoTelefonico) {
+        this.contattoTelefonico = contattoTelefonico;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAutomobile() {
+        return automobile;
+    }
+
+    public void setAutomobile(String automobile) {
+        this.automobile = automobile;
+    }
+
+    public int getNumPostiDisponibili() {
+        return numPostiDisponibili;
+    }
+
+    public void setNumPostiDisponibili(int numPostiDisponibili) {
+        this.numPostiDisponibili = numPostiDisponibili;
+    }
+
+    public List<EntityValutazione> getValutazioni() {
+        return valutazioni;
+    }
+
+    public void setValutazioni(List<EntityValutazione> valutazioni) {
+        this.valutazioni = valutazioni;
+    }
+
+    public List<EntityPrenotazione> getPrenotazioni() {
+        return prenotazioni;
+    }
+
+    public void setPrenotazioni(List<EntityPrenotazione> prenotazioni) {
+        this.prenotazioni = prenotazioni;
+    }
+
+    public List<EntityViaggio> getViaggiCondivisi() {
+        return viaggiCondivisi;
+    }
+
+    public void setViaggiCondivisi(List<EntityViaggio> viaggiCondivisi) {
+        this.viaggiCondivisi = viaggiCondivisi;
     }
 }
