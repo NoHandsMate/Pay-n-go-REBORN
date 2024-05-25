96451d6237312c26a46ebb2aa56a86dcc01067b4
NoHandsMate <z170s.lol@gmail.com>
2024-05-25 10:47:25 +0200

    [Package Entity]: Aggiunto lo scheletro delle classi facade

---
+++ src/entity/EntityControllerCarpooling.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class EntityControllerCarpooling {
+
+    public boolean generaReportIncassi() {
+        return false;
+    }
+
+    public boolean generaReportUtenti() {
+        return false;
+    }
+}
---
+++ src/entity/EntityControllerUtente.java
@@ -0,0 +1,36 @@
+package entity;
+
+public class EntityControllerUtente {
+
+    public boolean registraUtente() {
+        return false;
+    }
+
+    public boolean loginUtente() {
+        return false;
+    }
+
+    public boolean convidiViaggio() {
+        return false;
+    }
+
+    public boolean aggiornaDatiPersonali() {
+        return false;
+    }
+
+    public boolean visualizzaPrenotazioni() {
+        return false;
+    }
+
+    public boolean gestisciPrentonazioni() {
+        return false;
+    }
+
+    public boolean ricercaViaggio() {
+        return false;
+    }
+
+    public boolean valutaUtente() {
+        return false;
+    }
+}
