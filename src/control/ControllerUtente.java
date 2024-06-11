d31353bf59a42c3b432bd5d8e796848891d3f729
NoHandsMate <z170s.lol@gmail.com>
2024-06-11 11:09:52 +0200

    Preparazione ControllerUtente

--- src/control/ControllerUtente.java
+++ src/control/ControllerUtente.java
@@ -3,6 +3,7 @@ package control;
 
 import java.util.AbstractMap;
 import entity.FacadeEntityUtente;
+import exceptions.LoginUserException;
 import exceptions.RegistrationFailedException;
 
 public class ControllerUtente {
@@ -25,13 +26,20 @@ public class ControllerUtente {
                                                                           String telefono) {
 
         try {
-
             FacadeEntityUtente.getInstance().registraUtente(nome, cognome, email, auto, password, postiDisp, telefono);
         } catch (RegistrationFailedException e) {
             return new AbstractMap.SimpleEntry<>(false, e.getMessage());
         }
 
         return new AbstractMap.SimpleEntry<>(true, "Registrazione effettuata con successo");
+    }
 
+    public AbstractMap.SimpleEntry<Boolean, String> loginUtente(String email, char[] password) {
+
+        try {
+            FacadeEntityUtente.getInstance().loginUtente(email, password);
+        } catch (LoginUserException e) {
+            return new AbstractMap.SimpleEntry<>(false, e.getMessage());
+        }
     }
 }
